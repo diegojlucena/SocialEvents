1c091b614d6214697485d927bf6b5327c1c70e04
Diego Lucena <diegojlucena@gmail.com>
2015-11-12 13:13:48 -0200

    adding metadata

---
+++ Metadata/SocialEventListMetaData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using MrCMS.Entities.Documents;
+using MrCMS.Entities.Documents.Metadata;
+using MrCMS.Web.Apps.SocialEvents.Pages;
+
+namespace MrCMS.Web.Apps.SocialEvents.Metadata
+{
+    public class SocialEventListMetaData : DocumentMetadataMap<SocialEventList>
+    {
+        public override string WebGetController
+        {
+            get { return "SocialEvent"; }
+        }
+        public override string IconClass
+        {
+            get { return "glyphicon glyphicon-list"; }
+        }
+        
+        public override ChildrenListType ChildrenListType
+        {
+            get { return ChildrenListType.WhiteList; }
+        }
+
+        public override IEnumerable<Type> ChildrenList
+        {
+            get { yield return typeof(Pages.SocialEvent); }
+        }
+
+        public override string DefaultLayoutName
+        {
+            get { return "Two Column"; }
+        }
+
+        public override SortBy SortBy
+        {
+            get
+            {
+                return SortBy.PublishedOn;
+            }
+        }
+
+        public override int MaxChildNodes
+        {
+            get { return 5; }
+        }
+    }
+}
+
---
+++ Metadata/SocialEventMetaData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using MrCMS.Entities.Documents;
+using MrCMS.Entities.Documents.Metadata;
+using MrCMS.Web.Apps.SocialEvents.Pages;
+
+namespace MrCMS.Web.Apps.SocialEvents.Metadata
+{
+    public class SocialEventMetaData : DocumentMetadataMap<SocialEvent>
+    {
+        public override string IconClass
+        {
+            get { return "glyphicon glyphicon-time"; }
+        }
+
+        public override ChildrenListType ChildrenListType
+        {
+            get { return ChildrenListType.WhiteList; }
+        }
+
+        public override bool RequiresParent
+        {
+            get { return true; }
+        }
+
+        public override bool AutoBlacklist
+        {
+            get { return true; }
+        }
+
+        public override string DefaultLayoutName
+        {
+            get { return "Two Column"; }
+        }
+        
+        public override bool Sortable
+        {
+            get { return false; }
+        }
+
+        public override IEnumerable<Type> ChildrenList
+        {
+            get { yield break; }
+        }
+
+        public override bool RevealInNavigation
+        {
+            get { return false; }
+        }
+    }
+}
+
